a8101504e31e57776976b37a23dfcb40a95596bf
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-06-28 16:47:41 +0200

    Added Tests

--- src/test/java/com/yuliya1303/tests/AllureExamplesRepositoryTests.java
+++ src/test/java/com/yuliya1303/tests/AllureExamplesRepositoryTests.java
@@ -2,9 +2,6 @@ package com.yuliya1303.tests;
 
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Owner;
-import io.qameta.allure.Step;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +16,15 @@ public class AllureExamplesRepositoryTests {
     void watcherShouldExist() {
         step("Open https://github.com/Yuliya1303/Allure_Examples");
         step("Verify that 1 watcher exists");
+    }
+
+    @Test
+    @AllureId("10706")
+    @DisplayName("Allure examples repository exists in Yuliya1303 profile")
+    @Owner("allure8")
+    void repositoryShouldExistsInSearchResults() {
+        step("Open https://github.com/Yuliya1303?tab=repositories");
+        step("Enter Allure_Examples in Search field");
+        step("Verify that Allure_Examples text exists in search results");
     }
 }
